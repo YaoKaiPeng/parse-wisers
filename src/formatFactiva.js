12f0c43a13c3aa96f330bab8adcaf24ee849f2ef
kaipeng.yao <kaipeng.yao@e-research-solutions.com>
2020-05-29 12:12:25 +0800

    update

---
+++ src/formatFactiva.js
@@ -0,0 +1,33 @@
+const moment = require("moment");
+const cheerio = require("cheerio");
+
+module.exports = function(html) {
+  const list = html.split(/<p>文件 .+?<\/p>/);
+  list.pop();
+  const results = list.map((item, index) => {
+    let _item = item;
+    if (item.startsWith("<p></p>")) {
+      _item = _item.replace("<p></p>", "");
+    }
+    const [, title, infos, ..._content] = _item.split(/<p><\/p>/);
+    const titleDom = cheerio.load(title, { decodeEntities: false });
+    const infoDom = cheerio.load(infos, { decodeEntities: false });
+    const date = infoDom("p:nth-child(3)").text();
+    return {
+      date: moment(date, "YYYY 年 MM 月 DD 日 HH:mm").format(
+        "YYYY-MM-DD HH:mm"
+      ),
+      from: infoDom("p:nth-child(4)").text(),
+      in: titleDom("p:nth-child(1)").text(),
+      by: infoDom("p:nth-child(1)")
+        .text()
+        .replace("By ", "")
+        .trim(),
+      title: titleDom("p strong").text(),
+      content: _content.join("")
+    };
+  });
+  die(results);
+  // console.log(results);
+  return results;
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-var formatText = require('./formatText.js')
-var formatDoc = require('./formatDoc.js')
-var formatHtml = require('./formatHtml.js')
+var formatText = require("./formatText.js");
+var formatDoc = require("./formatDoc.js");
+var formatHtml = require("./formatHtml.js");
+var formatFactiva = require("./formatFactiva");
 
 module.exports = {
   formatText,
   formatDoc,
   formatHtml,
   parseWisers: function({ text, type }) {
-    if (type == 'TXT') {
-      return formatText(text)
+    if (type == "TXT") {
+      return formatText(text);
     }
-    if (type == 'DOC') {
-      return formatDoc(text)
+    if (type == "DOC") {
+      return formatDoc(text);
     }
-    if (type == 'HTML') {
-      return formatHtml(text)
+    if (type == "HTML") {
+      return formatHtml(text);
     }
-    return new Error('Type is Error. Please choose between TXT, DOC and HTML.')
-  },
-}
+    if (type == "factiva") {
+      return formatFactiva(text);
+    }
+    return new Error("Type is Error. Please choose between TXT, DOC and HTML.");
+  }
+};
