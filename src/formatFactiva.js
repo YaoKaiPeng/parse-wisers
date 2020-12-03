5921de199c0ccc32cccfad9796f2f91960efd8a9
kaipeng.yao <kaipeng.yao@e-research-solutions.com>
2020-12-03 16:41:50 +0800

    fix: 修复解析factiva的数据错误

--- src/formatFactiva.js
+++ src/formatFactiva.js
@@ -2,30 +2,38 @@ const moment = require("moment");
 const cheerio = require("cheerio");
 
 module.exports = function(html) {
-  const list = html.split(/<p>文件 .+?<\/p>/);
-  list.pop();
+  const list = html
+    .replace(
+      /<p><a id=(.*?)<\/a>/g,
+      "<a data-id='ers' id=$1</a><p><a id=$1</a>"
+    )
+    .split(/<a data-id='ers' id=.*?<\/a>/);
+  list.shift();
   const results = list.map((item, index) => {
-    let _item = item;
+    let [_item] = item.split(/<p>文件 .*?<\/p>/);
     if (item.startsWith("<p></p>")) {
       _item = _item.replace("<p></p>", "");
     }
     if (!_item.startsWith("<p><img")) {
-      _item = item;
+      _item = _item;
     }
     if (_item.startsWith("<p> </p>")) {
       _item = _item.replace("<p> </p>", "");
     }
-    let [logo, title, infos, ..._content] = _item.split(/<p><\/p>/);
-    const logoDom = cheerio.load(logo, { decodeEntities: false });
-    if (logoDom.text().trim()) {
-      const [_title, _infos, ..._content1] = _item.split(/<p><\/p>/);
-      title = _title;
-      infos = _infos;
-      _content = _content1;
-    }
+    let [title, infos, ..._content] = _item.split(/<p><\/p>/);
+
     const titleDom = cheerio.load(title, { decodeEntities: false });
     const infoDom = cheerio.load(infos, { decodeEntities: false });
-    const date = infoDom("p:nth-child(3)").text();
+    let date;
+    infos.split(/<\/p>/).forEach(info_item => {
+      if (
+        info_item.length > 4 &&
+        info_item.includes("年") &&
+        info_item.includes("月")
+      ) {
+        date = info_item.replace(/<p>/, "").trim();
+      }
+    });
     return {
       date: moment(date, "YYYY 年 MM 月 DD 日 HH:mm").format(
         "YYYY-MM-DD HH:mm"
