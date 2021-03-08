5659f414df5984674ac6a78c5194c81de2ed2964
kaipeng.yao <kaipeng.yao@e-research-solutions.com>
2021-03-08 15:46:06 +0800

    🐞fix(0.0.20):修复factiva数据解析兼容

--- src/formatFactiva.js
+++ src/formatFactiva.js
@@ -1,31 +1,33 @@
 const moment = require("moment");
 const cheerio = require("cheerio");
 
-module.exports = function(html) {
+module.exports = function (html) {
   const list = html
     .replace(
       /<p><a id=(.*?)<\/a>/g,
       "<a data-id='ers' id=$1</a><p><a id=$1</a>"
     )
     .split(/<a data-id='ers' id=.*?<\/a>/);
-  list.shift();
+  if (list.length > 1) {
+    list.shift();
+  }
+
   const results = list.map((item, index) => {
     let [_item] = item.split(/<p>文件 .*?<\/p>/);
-    if (item.startsWith("<p></p>")) {
-      _item = _item.replace("<p></p>", "");
+    if (item.startsWith("<p></p>") || _item.startsWith("<p> </p>")) {
+      _item = _item.replace(/<p><\/p>|<p> <\/p>/, "");
     }
-    if (!_item.startsWith("<p><img")) {
-      _item = _item;
+    if (_item.startsWith("<p><img")) {
+      _item = _item.replace(/<p><img.*?<\/p>/, "");
     }
-    if (_item.startsWith("<p> </p>")) {
-      _item = _item.replace("<p> </p>", "");
+    if (_item.startsWith("<p></p>") || _item.startsWith("<p> </p>")) {
+      _item = _item.replace(/<p><\/p>|<p> <\/p>/, "");
     }
     let [title, infos, ..._content] = _item.split(/<p><\/p>/);
-
     const titleDom = cheerio.load(title, { decodeEntities: false });
     const infoDom = cheerio.load(infos, { decodeEntities: false });
     let date;
-    infos.split(/<\/p>/).forEach(info_item => {
+    infos.split(/<\/p>/).forEach((info_item) => {
       if (
         info_item.length > 4 &&
         info_item.includes("年") &&
@@ -40,12 +42,9 @@ module.exports = function(html) {
       ),
       from: infoDom("p:nth-child(4)").text(),
       in: titleDom("p:nth-child(1)").text(),
-      by: infoDom("p:nth-child(1)")
-        .text()
-        .replace("By ", "")
-        .trim(),
+      by: infoDom("p:nth-child(1)").text().replace("By ", "").trim(),
       title: titleDom("p strong").text(),
-      content: _content.join("")
+      content: _content.join(""),
     };
   });
   return results;
